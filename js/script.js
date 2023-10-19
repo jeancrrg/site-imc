fe81cdb4ab533d37c6c7baff052b3607a0e22b25
jeancrrg <jeancrg3232@gmail.com>
2023-10-18 22:53:59 -0300

    Atualizado método de calcular o imc

--- js/script.js
+++ js/script.js
@@ -11,14 +11,26 @@ form.addEventListener('submit', function(event) {
     const imc = ((altura * altura) / peso).toFixed(2);
 
     const valorImc = document.getElementById('valor-imc');
-    let descricao = '';
+    let descricaoResultado = '';
 
     document.getElementById('resultado-imc').classList.remove('hidden');
 
     if (imc < 18.5) {
-        descricao = ''
-    } 
+        descricaoResultado = 'Cuidado! Você está abaixo do peso!'
+    } else if (imc >= 18.5 && imc <= 25) {
+        descricaoResultado = 'Parabéns! Você está no peso ideal!'
+    } else if (imc > 25 && imc <= 30) {
+        descricaoResultado = 'Cuidado! Você está com sobrepeso!'
+    } else if (imc > 30 && imc <= 35) {
+        descricaoResultado = 'Cuidado! Você está com obesidade moderada!'
+    } else if (imc > 35 && imc <= 40) {
+        descricaoResultado = 'Cuidade! Você está com obesidade severa!'
+    } else {
+        descricaoResultado = 'Cuidado! Você está com obesidade morbida!'
+    }
 
-    
+    valorImc.textContent = imc.replace('.', ',');
+    document.getElementById('descricao-resultado').textContent = descricaoResultado;
+
 
 });
